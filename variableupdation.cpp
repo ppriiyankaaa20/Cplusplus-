911e080bf69d0a0723a12cd535ef65afdee2ac63
ppriiyankaaa20 <priyankagowda491@gmail.com>
2026-03-20 00:44:28 +0530

    added variable updation program

---
+++ variableupdation.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    cout<<a<<endl; //here a is like a container it stores value 10 and prints it in output
+    a=90;
+    cout<<a<<endl; //when again given variable a it erases the first value and store this
+    return 0;
+}
+// taking user input⬇️
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     cout<<"x=";
+//     cin>>x;
+//     return 0;
+// }
