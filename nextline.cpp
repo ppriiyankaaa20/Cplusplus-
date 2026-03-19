37a13106f6de3ec9df50ba62e14c00713431e0aa
ppriiyankaaa20 <priyankagowda491@gmail.com>
2026-03-19 21:36:07 +0530

    added ways to code for nextline

---
+++ nextline.cpp
@@ -0,0 +1,18 @@
+// ways in which i can show my instructions in next line
+// endl; which ends the line and takes the compiler to next line
+// "\n" works same as endl;
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"my name is priyanka" <<"\n";
+    cout<<"i am an engineering student";
+    return 0;
+}
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"my name is priyanka" <<endl;
+//     cout<<"i am an engineering student";
+//     return 0;
+// }
+// can also be programmed like above mentioned!!
