f2861693150146c0339fbd972ace4235446ca4dc
ppriiyankaaa20 <priyankagowda491@gmail.com>
2026-03-19 23:34:50 +0530

    added variable declaration and initialization

---
+++ variables.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double a=20;//here double is used to store decimals points of the value it can also be written as int and in the cout it should be (double) a/b
+    double b=50;
+    cout<<"a+b="<<a+b<<endl<<"a*b="<<a*b<<endl<<"a/b="<<a/b<<endl<<"a-b="<<a-b;
+    return 0;
+}
+// the process of creating variable is declaration
+// the process of storing value in variable is initialization
