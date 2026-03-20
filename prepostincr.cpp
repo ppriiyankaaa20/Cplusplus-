bc191d12b32655703e03d5288d208aec951b5c01
ppriiyankaaa20 <priyankagowda491@gmail.com>
2026-03-20 13:30:39 +0530

    added pre and post increment example

---
+++ prepostincr.cpp
@@ -0,0 +1,19 @@
+// #include<iostream> //this is pre increment example
+// using namespace std;
+// int main(){
+//     int P=8;
+//     cout<<P<<endl;
+//     cout<<++P<<endl;
+//     cout<<P<<endl;
+//     return 0;
+// }
+
+#include<iostream> //this is post increment example
+using namespace std;
+int main(){
+    int P=8;
+    cout<<P<<endl;
+    cout<<P++<<endl;
+    cout<<P<<endl;
+    return 0;
+}
