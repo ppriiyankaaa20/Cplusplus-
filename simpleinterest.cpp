49891c87f65a0cb75fca566cc28b95bff540b6ae
ppriiyankaaa20 <priyankagowda491@gmail.com>
2026-03-20 14:55:30 +0530

    modified repo by deleting exe files

---
+++ simpleinterest.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float P;
+    float R;
+    float T;
+    float SI;
+    cout<<"P=";
+    cin>>P;
+    cout<<"R=";
+    cin>>R;
+    cout<<"T=";
+    cin>>T;
+    cout<<"SI=";
+    cout<<(P*R*T)/100;
+    return 0;
+}
