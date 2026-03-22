02330e25b6a3c6715da96998dabd7305df56f466
ppriiyankaaa20 <priyankagowda491@gmail.com>
2026-03-22 22:36:38 +0530

    added odd or even program

---
+++ oddeven.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a num:";
+    cin>>n;
+    if(n%2==0){
+        cout<<"the entered num is even";
+    }
+    else{
+        cout<<"the entered num is odd";
+    }
+    return 0;
+}
