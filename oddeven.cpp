e85808b07bf1e55971e3580f40e548680847705f
ppriiyankaaa20 <priyankagowda491@gmail.com>
2026-03-23 19:25:54 +0530

    added odd or even program

--- oddeven.cpp
+++ oddeven.cpp
@@ -4,14 +4,16 @@ int main(){
     int n;
     cout<<"enter a num:";
     cin>>n;
-    if(n%2==0){
+    if(n%2==0)
         cout<<"the entered num is even";
-    }
-    else{
+    else
         cout<<"the entered num is odd";
-    }
     return 0;
 }
 //can also be written using only if statements for both conditions
 //if(n%2==0)
 //if(n%2!=0)
+//syntax: if(condition){
+//}
+//else{
+//}  can also be written without flower bracs
