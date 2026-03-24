dd94110acad433e8f1b1e62c5ab89f91879a1b73
ppriiyankaaa20 <priyankagowda491@gmail.com>
2026-03-24 20:25:54 +0530

    added number that is divisible by 5 and 3

---
+++ divisibleby5and3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter a:";
+    cin>>a;
+    if(a%5==0 && a%3==0){
+        cout<<"it is divisible by 5 and 3";
+    }
+    else{
+        cout<<"not divisible by 5 and 3";
+    }
+    return 0;
+}
