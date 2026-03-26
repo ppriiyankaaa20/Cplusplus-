ecec20e7aaa536efbbc54396ce5294c56fa072f2
ppriiyankaaa20 <priyankagowda491@gmail.com>
2026-03-26 23:01:35 +0530

    added natural number program

--- naturalno.cpp
+++ naturalno.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int sum=0;
+    cout<<"the first 10 natural numbers are:";
     for(int i=1;i<=10;i++){
         cout<<i<<endl;
+        sum=sum+i;
     }
+    cout<<"the sum of first 10 natural numbers are:";
+    cout<<sum;
     return 0;
 }
