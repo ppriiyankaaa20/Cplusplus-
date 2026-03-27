d07f387ac0c4e77106aa89594f5a347d5091fb01
ppriiyankaaa20 <priyankagowda491@gmail.com>
2026-03-27 14:35:02 +0530

    added arithmetic prgm

---
+++ AP.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //4 7 10 13 16 19...
+    int n;
+    cout<<"enter the num:";
+    cin>>n;
+    for(int i=4;i<=3*n-2;i+=3){
+        cout<<i<<" ";
+    }
+    return 0;
+}
+//this program is arithmatic progression..it is a sequence of num where the diff b/w consecutive terms is constant
+//an=a+(n-1)d this is the formula for nth term
