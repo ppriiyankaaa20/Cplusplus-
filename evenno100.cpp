d9d6a3457cfeaa9f2b6172658c887fe7bc9009fa
ppriiyankaaa20 <priyankagowda491@gmail.com>
2026-03-27 13:10:00 +0530

    added basic forloop pgm

---
+++ evenno100.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=100;i++){
+        if(i%2==0)
+        cout<<i<<endl;
+    }
+    return 0;
+}
+//for odd no just put if(i%2!=0) in the condition
+//can also modify increment
+//for(int i=2;i<=100;i=i+2){
+//       cout<<i<<endl;
+//  }
+//this time we no need to add the if condition
---
+++ tableof9.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=9;i<=90;i++){
+        if(i%9==0){
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}
